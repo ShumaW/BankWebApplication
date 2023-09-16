7367408b755d12179bd486183928bfa489cd9c8e
Yevgeniy_Shulimenko <evgeniy.shulimenko@gmail.com>
2023-09-16 23:52:54 +0200

    16.09.2023 insert to database information from sql file

---
+++ src/main/resources/db/changelog/changeset/v0.0.1-SNAPSHOT/managers.sql
@@ -0,0 +1,4 @@
+insert into managers (id, first_name, last_name, status, description, created_at)
+values ('1763f054-5393-11ee-8c99-0242ac120002', 'Sem', 'Smith', '0', '', '2023-07-07 08:41:08'),
+       ('caed40d0-5401-11ee-8c99-0242ac120002', 'Judith', 'Clasen', '0', '', '2023-07-15 08:41:08'),
+       ('f74cf29c-5401-11ee-8c99-0242ac120002', 'Eric', 'Rutten', '0', '', '2023-07-21 08:30:21');
